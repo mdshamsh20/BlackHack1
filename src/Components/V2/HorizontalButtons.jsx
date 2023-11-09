bc0011b70f5908677b09b30d54a1be80e4e5d924
Md Shamsuzzama Siddique <75604504+mdshamsh20@users.noreply.github.com>
2023-11-09 10:56:59 +0530

    HorizontalButtons.jsx

--- src/Components/V2/HorizontalButtons.jsx
+++ src/Components/V2/HorizontalButtons.jsx
@@ -5,20 +5,20 @@ const HorizontalScrollCards = () => {
     <div className="max-w-screen-xl mt-20 mx-auto p-4 overflow-hidden">
       <div className="flex space-x-4 overflow-x-auto p-4" style={{ animation: 'scroll 20s linear infinite' }}>
         <div >
-          <button className="px-16 py-2 text-lg   text-black bg-white hover:bg-cyan-300 rounded-3xl">Something</button>
+          <button className="px-16 py-2 text-lg   text-black bg-white hover:bg-cyan-300 rounded-3xl">XSS</button>
         </div>
         <div >
-          <button className="px-16 py-2 text-lg font-semibold text-black bg-white  hover:bg-cyan-300 rounded-3xl">Something</button>
+          <button className="px-16 py-2 text-lg font-semibold text-black bg-white  hover:bg-cyan-300 rounded-3xl">DDOS</button>
         </div>
         <div >
-          <button className="px-16 py-2 text-lg font-semibold text-black bg-white  hover:bg-cyan-300 rounded-3xl">Something</button>
+          <button className="px-16 py-2 text-lg font-semibold text-black bg-white  hover:bg-cyan-300 rounded-3xl">XSS2</button>
         </div>
 
         <div >
-          <button className="px-16 py-2 text-lg font-semibold text-black bg-white  hover:bg-cyan-300 rounded-3xl">Something</button>
+          <button className="px-16 py-2 text-lg font-semibold text-black bg-white  hover:bg-cyan-300 rounded-3xl">HACKER</button>
         </div>
         <div >
-          <button className="px-16 py-2 text-lg font-semibold text-black bg-white  hover:bg-cyan-300 rounded-3xl">Something</button>
+          <button className="px-16 py-2 text-lg font-semibold text-black bg-white  hover:bg-cyan-300 rounded-3xl">LeaderBord</button>
         </div>
        
       </div>
