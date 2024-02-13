305284d73e7bdf99776afe34fba10b2f6d614a92
Md Shamsuzzama Siddique <75604504+mdshamsh20@users.noreply.github.com>
2024-02-13 22:45:36 +0530

    Signup.jsx

--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -51,7 +51,7 @@ const Signup = () => {
                         <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required/>
                     </div>
                     <div>
-                        <button type="submit" name="submit" id="submit" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">Submit</button>
+                        <button type="submit" name="submit" id="submit" className="bg-gray-50  text-black border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">Submit</button>
                     </div>
                     <div className="flex items-start">
                         <div className="flex items-center h-5">
